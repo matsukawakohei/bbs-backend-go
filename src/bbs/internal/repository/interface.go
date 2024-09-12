5bf57a0c71f5d52290b491a3a3ee1c4e34643bc3
Kohei Matsukawa <kohei.mattsukawa@gmail.com>
2024-09-12 21:35:05 +0900

    [fix] RepositoryのInterfaceを分離

---
+++ src/bbs/internal/repository/interface.go
@@ -0,0 +1,24 @@
+package repository
+
+import "bbs/internal/model"
+
+type IAuthRepository interface {
+	CreateUser(user model.User) error
+	FindUser(email string) (*model.User, error)
+}
+
+type IThreadRepository interface {
+	Create(newThread model.Thread) (*model.Thread, error)
+	Update(updateThread model.Thread) (*model.Thread, error)
+	Delete(threadId uint, userId uint) error
+	FindAll() (*[]model.Thread, error)
+	FindById(threadId uint) (*model.Thread, error)
+}
+
+type ICommentRepository interface {
+	Create(newComment model.Comment) (*model.Comment, error)
+	FindByThreadId(threadId uint, userId uint) (*[]model.Comment, error)
+	FindById(id uint, threadId uint, userId uint) (*model.Comment, error)
+	Update(updateComment model.Comment) (*model.Comment, error)
+	Delete(id uint, threadId uint, userId uint) error
+}
