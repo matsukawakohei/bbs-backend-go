400fc8424e7cb93f1299564a39c87d473c530ee8
Kohei Matsukawa <kohei.mattsukawa@gmail.com>
2024-09-16 00:48:45 +0900

    [add] ModelにJsonタグを追加

--- src/bbs/internal/model/comment.go
+++ src/bbs/internal/model/comment.go
@@ -4,7 +4,7 @@ import "gorm.io/gorm"
 
 type Comment struct {
 	gorm.Model
-	Body     string `gorm:"not null"`
-	UserID   uint   `gorm:"not null"`
-	ThreadID uint   `gorm:"not null"`
+	Body     string `gorm:"not null" json:"body"`
+	UserID   uint   `gorm:"not null" json:"userId"`
+	ThreadID uint   `gorm:"not null" json:"threadId"`
 }
